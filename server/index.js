e2d050fb8175856052916d6dec9296097819728d
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-27 17:13:08 -0300

    using node and ENV

--- server/index.js
+++ server/index.js
@@ -1,113 +1,45 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
+require('dotenv').config()
 
 // Create the server
 const app = express()
+const path = require('path')
 
-// Connect to the PSQL Heroku DB
-const { Pool } = require('pg');
-connectionString = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-  };
-const pool = new Pool(connectionString)
+// use middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//load routes
+const proveedores = require('./routes/proveedores')
+const users = require('./routes/users')
+const userToken = require('./routes/token')
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '/../client/build')));
 
-app.get('/api/', cors(), async (req, res, next) => {
+// Serve our base route that returns a Hello World cow
+app.get('/api/status', cors(), async (req, res, next) => {
+  console.log(process.env.DATABASE_URL)
   try {
-    const result = 'API Wilson-tesoreria'
-    res.json({ result })
+    const result = 'Connected'
+    res.status(200).json({ message: result })
   } catch (err) {
     next(err)
   }
-})
-
-app.get('/api/facturas', cors(), async (req, res, next) => {
-  try {
-    pool.query('SELECT * FROM facturas;', (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-})
-
-app.get('/api/empresas', cors(), async (req, res, next) => {
-  try {
-    pool.query('SELECT * FROM empresas;', (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-})
-
-app.get('/api/facturas/:id', cors(), async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id, 10);
-    console.log(id)
-    pool.query('SELECT * FROM facturas WHERE id=$1;', [id], (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-}) 
-
-
-app.get('/api/proveedores/:id', cors(), async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id, 10);
-    console.log(id)
-    pool.query('SELECT * FROM proveedores WHERE id=$1;', [id], (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-}) 
-
-app.get('/api/empresas/:id', cors(), async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id, 10);
-    console.log(id)
-    pool.query('SELECT * FROM empresas WHERE id=$1;', [id], (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-}) 
-
-app.get('/api/proveedores', cors(), async (req, res, next) => {
-  try {
-    pool.query('SELECT id, nombre FROM proveedores;', (err, result) => {
-      if (err) throw err;
-      values = result.rows
-      res.json(values)
-    });
-  } catch (err) {
-    next(err)
-  }
-})
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
+
+// general routes
+})
+app.use('/api/proveedores', cors(), proveedores)
+app.use('/api/users', cors(), users)
+app.use('/api/user_token', cors(), userToken)
 
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
