f5255b0f7c98e2cd0d51a560deffb36dfc8febd3
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-28 16:44:18 -0300

    creating proveedores routes

--- server/controller/proveedores.js
+++ server/controller/proveedores.js
@@ -1,6 +1,6 @@
 const { pool } = require('../db/dbConnection')
 
-const getProveedores = async (req, res) => {
+const listProveedores = async (req, res) => {
     try {
       pool.query('SELECT * FROM proveedores;', (err, result) => {
         if (err) res.status(404).json({error: 'Not provider found'});
@@ -11,8 +11,18 @@ const getProveedores = async (req, res) => {
     }
   }
 
-
+const createProveedores = (req, res) => {
+  try {
+    pool.query('INSERT INTO proveedores(name) VALUES($1) RETURNING *', [req.body.name], (err, result)=>{
+      if (err) res.status(404).json({error: err});
+        res.status(202).json({message: 'user created', id: result.rows})
+    })
+  } catch(err) {
+    res.status(500).json({ err: err })
+  }
+}
 
 module.exports = {
-    getProveedores
+    listProveedores,
+    createProveedores
 }
--- server/routes/proveedores.js
+++ server/routes/proveedores.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const proveedoresController = require('../controller/proveedores')
+const { proveedoresValidationRules, validate } = require('../helpers/validators/proveedores')
 
-router.get('/', proveedoresController.getProveedores)
+router.get('/', proveedoresController.listProveedores)
+router.post('/', proveedoresValidationRules(), validate, proveedoresController.createProveedores)
 
   module.exports = router
