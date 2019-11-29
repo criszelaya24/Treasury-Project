fb9559da8c368ce072f2e453c8f58620f9b30e17
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-29 15:40:34 -0300

    changing status proveedores response

--- server/controller/proveedores.js
+++ server/controller/proveedores.js
@@ -4,10 +4,10 @@ const listProveedores = async (req, res) => {
     try {
       pool.query('SELECT * FROM proveedores;', (err, result) => {
         if (err) res.status(404).json({error: 'Not provider found'});
-        res.status(200).json({data: result.rows})
+        res.status(200).json({message: "All proveedores", data: result.rows})
       });
     } catch (err) {
-        res.status(404).json({error: err})
+        res.status(500).json({error: err})
     }
   }
 
@@ -15,7 +15,7 @@ const createProveedores = async (req, res) => {
   try {
     pool.query('INSERT INTO proveedores(name) VALUES($1) RETURNING *', [req.body.name], (err, result)=>{
       if (err) res.status(404).json({error: err});
-        res.status(202).json({message: 'proveedor created', id: result.rows})
+        res.status(201).json({message: 'proveedor created', proveedor: result.rows[0]})
     })
   } catch(err) {
     res.status(500).json({ err: err })
