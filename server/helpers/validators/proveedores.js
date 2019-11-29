d2d1aa2ff92fb6c176d5e67770624cd4dd3ad551
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-29 16:23:20 -0300

    reusing proovedores

--- server/helpers/validators/proveedores.js
+++ server/helpers/validators/proveedores.js
@@ -1,10 +1,17 @@
-const { body } = require('express-validator')
+const { body, param } = require('express-validator')
 const proveedoresValidationRules = () => {
   return [
     body('name').not().isEmpty().withMessage('Name must me fill').isLength({min: 5}).withMessage('Lenght of name needs to be up of 5')
   ]
 }
 
+const proveedoresSearchRules = () => {
+  return [
+    param('id').not().isEmpty().withMessage('id must be pass')
+  ]
+}
+
 module.exports = {
   proveedoresValidationRules,
+  proveedoresSearchRules
 }
--- server/routes/proveedores.js
+++ server/routes/proveedores.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const proveedoresController = require('../controller/proveedores')
-const { proveedoresValidationRules } = require('../helpers/validators/proveedores')
+const { proveedoresValidationRules, proveedoresSearchRules } = require('../helpers/validators/proveedores')
 const { isUserSignIn } = require('../helpers/validators/users')
 const { validate } = require('../helpers/validateResults')
 
 router.get('/', isUserSignIn, proveedoresController.listProveedores)
 router.post('/', isUserSignIn, proveedoresValidationRules(), validate, proveedoresController.createProveedores)
+router.get('/:id',proveedoresSearchRules(), validate, proveedoresController.findProveedores)
 
   module.exports = router
