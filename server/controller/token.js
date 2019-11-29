f132476ececf83237e272086f472b1c65d14229d
Cristopher Palacios <criszelaya24@hotmail.com>
2019-11-29 15:00:26 -0300

    token route and controller

---
+++ server/controller/token.js
@@ -0,0 +1,29 @@
+const { pool } = require('../db/dbConnection')
+const bcrypt = require('bcrypt')
+const validatorUser = require('../helpers/validators/users')
+
+const createToken = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        pool.query('SELECT * FROM USERS where email = $1', [email], (err, result)=>{
+            if (err) res.status(404).json({message: "User Don't found"});
+            bcrypt.compare(password, result.rows[0].password).then(match => {
+                if (match){
+                    const user = {
+                        email: result.rows[0].email,
+                        typeUserId: result.rows[0].type_user_id
+                    }
+                    const token = validatorUser.generateToken(user);
+                    res.status(200).json({message: "User Loged in", token: token})
+                }
+                res.status(401).json({message: "Wrong password"})
+            })
+        })
+    }catch(err){
+        res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+module.exports = {
+    createToken
+}
