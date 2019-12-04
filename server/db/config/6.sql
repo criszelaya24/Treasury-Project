c0a051c307c03e2c1635f4b6bf78f137c3546556
Cristopher Palacios <criszelaya24@hotmail.com>
2019-12-04 14:55:39 -0300

    queries

--- server/db/config/6.sql
+++ server/db/config/6.sql
@@ -1,2 +1,1 @@
-CREATE TABLE facturas (id SERIAL PRIMARY KEY, numero int NOT NULL, proveedor int REFERENCES proveedores(id), currency VARCHAR(4), monto numeric, detalle VARCHAR(100), fecha date, vencimiento date, status varchar(50)  default 'por pagar', empresa int REFERENCES empresas(
-id), unique(numero, proveedor));
+CREATE TABLE currencies ( id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL UNIQUE, symbol VARCHAR(5) NOT NULL UNIQUE);
